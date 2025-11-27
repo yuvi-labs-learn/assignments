3f5f0c9cd4db7b5768367b764c20e09d2ac81dd9
Yuvarajaa <16523925+yuva-dev@users.noreply.github.com>
2025-11-27 10:20:04 +0530

    ended

--- ajay_kannan/data.js
+++
@@ -1,6 +0,0 @@
-const DATA = {
-  "name": "Ajay Kannan",
-  "uiLink": "https://dribbble.com/shots/25304102-Ecommerce-App",
-  "startDate": "Oct 28, 2025",
-  "endDate": "Nov 4, 2025"
-};
--- deepak/data.js
+++
@@ -1,6 +0,0 @@
-const DATA = {
-  "name": "Deepak Kumar",
-  "uiLink": "https://dribbble.com/shots/25653833-Fitness-App-UI-Concept",
-  "startDate": "Oct 28, 2025",
-  "endDate": "Nov 4, 2025"
-};
