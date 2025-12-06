5449d179105cc01e3642c6b3f1bb9f42674f26b2
Yuvarajaa <16523925+yuva-dev@users.noreply.github.com>
2025-12-06 10:42:49 +0530

    Quick commit

--- nandha/logeshwaran/data.js
+++ nandha/logeshwaran/data.js
@@ -1,6 +1,5 @@
 const DATA = {
   "name": "Logeshwaran",
-  "uiLink": "https://dribbble.com/shots/25653833-Fitness-App-UI-Concept",
   "startDate": "Dec 8, 2025",
   "endDate": "Dec 15, 2025"
 };
--- nandha/nandha_kumar/data.js
+++ nandha/nandha_kumar/data.js
@@ -1,6 +1,5 @@
 const DATA = {
   "name": "Nandha Kumar",
-  "uiLink": "https://dribbble.com/shots/25989629-Travel-mobile-app",
   "startDate": "Dec 8, 2025",
   "endDate": "Dec 15, 2025"
 };
